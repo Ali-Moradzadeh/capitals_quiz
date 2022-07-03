e90960b06df27b8c7412bd84b00eb5144ba34222
Ali-Moradzadeh <moraCode.master@gmail.com>
2022-07-04 01:35:07 +0430

    complete database type handling by add choice to user to choose database type and tables

--- check_db_type.py
+++ check_db_type.py
@@ -1,32 +1,39 @@
-import confirm_identification as txtDb
+import os
 import mysql.connector as connector
+import confirm_identification as txtDb
+import server_db_handle
+import data_handling
 
 __host = "host"
 __username = "user"
 __password = "password"
 __database = "database"
 __table = "table"
-
 __infos = (__host, __username, __password, __database, __table)
+__db_handling_obj = None
+__db = None
+__detailes = None
 
 def __getDbInfos() :
 	
-	return {x : input(f"{x} : ") for x in __infos}
+	global __detailes
+	__detailes = {x : input(f"{x} : ") for x in __infos}
 	
-
 def checkConnection() :
 	
-	infos = __getDbInfos()
+	__getDbInfos()
 	
 	try :
 		
-		connector.connect(host = infos[__host], user = infos[__username], password =  infos[__password], database =  infos[__database])
+		global __db
+		__db = connector.connect(host = __detailes[__host], user = __detailes[__username], password =  __detailes[__password], database =  __detailes[__database])
 		
+		print("\nconnect successfully.\n")
 		return True
 		
-	except ConnectionError :
+	except :
 		
-		print("connection failed")
+		print("\ndatabase connection failed\n")
 		return False
 
 def whereToSaveDatas() :
@@ -37,14 +44,29 @@ def whereToSaveDatas() :
 		
 		if checkConnection() :
 			
-			pass
+			go_on = input("Continue ?? (y for yes anyother for no) : ").lower()
+			__db_handling_obj = server_db_handle.Data_handling(__detailes, __db)
+		
+			if go_on == "y" and __db_handling_obj.checkValidTable():
+				
 			
+				txtDb.start(__db_handling_obj)
+			
+			else :
+				
+				whereToSaveDatas()
+			
+		else :
+			
+			whereToSaveDatas()
 			
 	elif haveDb == "n" :
 		
 		print("players informations have been saved in 'Database.txt' file in project main directory.")
-		txtDb.start()
 		
+		__db_handling_obj = data_handling.Data_handling()
+		
+		txtDb.start(__db_handling_obj)
 	else :
 		print("invalid input. try again")
 		whereToSaveDatas()
