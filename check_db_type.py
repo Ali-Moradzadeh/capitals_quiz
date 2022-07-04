a633059e0f537300a74eb40f832d05a83a2ed5d4
Ali-Moradzadeh <moraCode.master@gmail.com>
2022-07-04 19:25:12 +0430

    changes some functions in check_db_type.py & server_database_handling.py file to been synchronize with all_server_dbs_handle.py file

--- check_db_type.py
+++ check_db_type.py
@@ -1,8 +1,7 @@
-import os
-import mysql.connector as connector
-import confirm_identification as txtDb
-import server_database_handling
-import local_database_handling
+import confirm_identification as confirmID
+import server_database_handling as server_db_handling
+import local_database_handling as local_db_handling
+import all_server_dbs_handle as allDBsHandle
 
 __host = "host"
 __username = "user"
@@ -13,47 +12,33 @@ __infos = (__host, __username, __password, __database, __table)
 __db_handling_obj = None
 __db = None
 __detailes = None
+__DBsHandle = None
 
 def __getDbInfos() :
 	
 	global __detailes
 	__detailes = {x : input(f"{x} : ") for x in __infos}
 	
-def checkConnection() :
-	
-	__getDbInfos()
-	
-	try :
-		
-		global __db
-		__db = connector.connect(host = __detailes[__host], user = __detailes[__username], password =  __detailes[__password], database =  __detailes[__database])
-		
-		print("\nconnect successfully.\n")
-		return True
-		
-	except :
-		
-		print("\ndatabase connection failed\n")
-		return False
-
 def whereToSaveDatas() :
 	
 	haveDb = input("do you have any server side databade to save account informations ?? (y/n) : ").lower()
 
 	if haveDb == "y" :
+		__getDbInfos()
+		__DBsHandle = allDBsHandle.Server_Database_Handling(__detailes[__host], __detailes[__username],__detailes[__password], __detailes[__database])
 		
-		if checkConnection() :
-			
+		if __DBsHandle.checkConnection() :
+			print("Successfully Connected.")
 			go_on = input("Continue ?? (y for yes anyother for no) : ").lower()
-			__db_handling_obj = server_database_handling.Data_handling(__detailes, __db)
+			__db_handling_obj = server_db_handling.Data_handling(__detailes[__table], __DBsHandle)
 		
-			if go_on == "y" and __db_handling_obj.checkValidTable():
+			if go_on == "y" and __db_handling_obj.isTableValid():
 				
 			
-				txtDb.start(__db_handling_obj)
+				confirmID.start(__db_handling_obj)
 			
 			else :
-				
+				print("connection falied.")
 				whereToSaveDatas()
 			
 		else :
@@ -64,9 +49,9 @@ def whereToSaveDatas() :
 		
 		print("players informations have been saved in 'Database.txt' file in project main directory.")
 		
-		__db_handling_obj = local_database_handling.Data_handling()
+		__db_handling_obj = local_db_handling.Data_handling()
 		
-		txtDb.start(__db_handling_obj)
+		confirmID.start(__db_handling_obj)
 	else :
 		print("invalid input. try again")
 		whereToSaveDatas()
--- server_database_handling.py
+++ server_database_handling.py
@@ -1,7 +1,5 @@
-#import mysql.connector as connector
-
 class Data_handling :
-	
+
 	__used_column_names = []
 	__column_username = "username"
 	__column_email = "email"
@@ -9,175 +7,95 @@ class Data_handling :
 	__column_score = "score"
 
 	__columns = [__column_username,__column_email, __column_password, __column_score]
-	
+
 	__columns_index = {__column_username : 0, __column_email : 1, __column_password : 2, __column_score :3}
-	
-	def __init__(self, detailes, db) :
-		
-		self.__db = db
-		self.__detailes = detailes
-		self.__cursor = db.cursor()
-		self.__table = detailes["table"]
-		self.__database = detailes["database"]
+
+	def __init__(self, table, allDBsHandleObj) :
+
+		self.__serverDBsHand = allDBsHandleObj
+		self.__db = allDBsHandleObj.getDB()
+		self.__cursor = self.__db.cursor()
+		self.__table = table
+		self.__column_names = None
+		self.__dict_k_key_v_columnNames = None
 
 
-	def __checkTableExist(self) :
-		self.__cursor.execute("show tables")
-		tables = self.__cursor.fetchall()		
-		result = False
-
-		for table in tables :
-			result = self.__table in table
-
-		return result
-
-
-	def checkValidTable(self) :
-		if not self.__checkTableExist() :
+	def isTableValid(self) :
+		if not self.__serverDBsHand.tableExist(self.__table) :
 			print("table not found.")
 			return False
 
-		query = f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='{self.__database}' AND TABLE_NAME='{self.__table}'"
-		
-		self.__cursor.execute(query)
-		fet = self.__cursor.fetchall()
-		
-		self.__column_names = tuple(x[0] for x in fet)
-		
+		self.__column_names = self.__serverDBsHand.getColumns(self.__table)
+
 		print(f"\ncolumn names are : {self.__column_names}\n")
-		
-		result = len(self.__column_names) == 4
-		if not result :
-			
-			print("Error. table must have exactly 4 columns")
-			return result
-		
-		columns = {key : {"column_name" : self.__controlTypedColumnName(key), "column_index" : self.__controlTypedColumnIndex() - 1} for key in self.__columns}
-			
-		self.__column_names = [V["column_name"] for K,V in columns.items()]
 
-		index = lambda key : tuple(self.__columns.index(K) for K,V in columns.items() if V["column_name"] == key)[0]
+		if not len(self.__column_names) == 4 :
 
-		self.__column_names.sort(key = index)
-		
-		
-		
-		self.__column_username, self.__column_email, self.__column_password, self.__column_score = self.__column_names
-			
-		return result
-		
-	def __controlTypedColumnName(self, key) :
-		
-		typedName = input(f"column name for storing {key} : ")
-		
-		if typedName in self.__column_names and typedName not in self.__used_column_names :
-			
-			self.__used_column_names.append(typedName)
-			return typedName
-			
-		elif typedName not in self.__column_names :
-			
-			print("please type correct column name , which you type is not a column name in table")
-	
-		else : 
-			print("this column has already been used try another one")
-		
-		self.__controlTypedColumnName(key)
-		
-		
-	def __controlTypedColumnIndex(self) :
-		
-		indexStr = input("this column's number in table : ")
-		
-		try :
-			
-			index = int(indexStr)
-			
-			if index < 1 or index > 4 :
-				
-				print("please insert just 1 or 2 or 3 or 4")
-				return self.__controlTypedColumnIndex()
-		
-			else :
-				return index
-			
-		except ValueError :
-			
-			print("please insert just 1 or 2 or 3 or 4")
-			return self.__controlTypedColumnIndex()
-	
-	
+			print("table must have exactly 4 columns")
+			return False
+
+		self.__dict_k_key_v_columnNames = self.__serverDBsHand.defineEachColumnStoreWhatData(self.__table, self.__columns)
+
+		return True
+
+
 	def registerPlr(self, username, email, password) :
-	
 		result = True
-		
-		if not (self.infoExist(username, self.__column_username) or self.infoExist(email, self.__column_email)) :
-		
-			query = f"insert into {self.__table}({self.__column_username}, {self.__column_email}, {self.__column_password}, {self.__column_score}) values ('{username}', '{email}', '{password}', 0)"
-	
+		if not (self.infoExist(username, self.__dict_k_key_v_columnNames[self.__column_username]) or self.infoExist(email, self.__dict_k_key_v_columnNames[self.__column_email])) :
+
+			query = f"insert into {self.__table}({self.__dict_k_key_v_columnNames[self.__column_username]}, {self.__dict_k_key_v_columnNames[self.__column_email]}, {self.__dict_k_key_v_columnNames[self.__column_password]}, {self.__dict_k_key_v_columnNames[self.__column_score]}) values ('{username}', '{email}', '{password}', 0)"
 			self.__cursor.execute(query)
 			self.__db.commit()
-		
-		elif self.infoExist(username, self.__column_username) :
-		
+
+		elif self.infoExist(username, self.__dict_k_key_v_columnNames[self.__column_username]) :
 			result = False
 			print("username already exist. try with another")
-		
-		elif self.infoExist(email, self.__column_email) :
-		
+
+		elif self.infoExist(email, self.__dict_k_key_v_columnNames[self.__column_email]) :
+
 			result = False
 			print("email already exist try with another")
-		
+
 		return result
-		
+
+
 	def getRecords(self) :
-	
 		query = f"select * from {self.__table}"
-	
 		self.__cursor.execute(query)
 		return self.__cursor.fetchall()
-	
-	
+
+
 	def recordsCount(self) :
 		return len(self.getRecords())
-	
-	
+
+
 	def getRowByRecord(self, value, key) :
-	
-		query = f"select * from {self.__table} where {key} = '{value}'"
-	
+		query = f"select * from {self.__table} where {self.__dict_k_key_v_columnNames[key]} = '{value}'"
 		self.__cursor.execute(query)
 		return self.__cursor.fetchall()[0]
-	
+
 
 	def infoExist(self, value, key) :
-	
-	#print(__cursor.column_names())
-		query = f"select {key} from {self.__table} where exists(select {key} from {self.__table} where {key} = '{value}')"
-	
+		query = f"select {self.__dict_k_key_v_columnNames[key]} from {self.__table} where exists(select {self.__dict_k_key_v_columnNames[key]} from {self.__table} where {self.__dict_k_key_v_columnNames[key]} = '{value}')"
 		self.__cursor.execute(query)
 		x = self.__cursor.fetchall()
-	
 		return len(x) != 0
-	
+
 
 	def allowedToLoginByEmail(self, value, password) :
-	
 		return self.__allowedToLoginByKey(value, self.__column_email, password)
 
 
 	def allowedToLoginByUsername(self,value, password) :
-	
 		return self.__allowedToLoginByKey(value, self.__column_username, password)
 
-	
+
 	def __allowedToLoginByKey(self, value, key, password) :
-	
+
 		result = self.infoExist(value, key) and self.getRowByRecord(value, key)[2] == password
 		if not result :
-			
 			print(f"{key} or password is incorrent.")
-			
+
 		return result
-			
+
 
